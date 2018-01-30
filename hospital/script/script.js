0c22c2d9400a9de9e545fb1b2a41d997cb44c249
niyasc <niyasmonc@gmail.com>
2018-01-31 00:30:06 +0530

    Add basic structure for hospital

---
+++ hospital/script/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		sectionsColor: ['#084C61', '#1D7874', '#D98324', '#eee'],
+		anchors:['home', 'experts', 'facilities', 'contact'],
+		menu: '#navMenu',
+		navigation: true
+	});
+});
